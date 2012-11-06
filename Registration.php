253fdf8b96594d029b2bd0c58e3f8475a5a5507a
Joe Cormier <jncormier24@gmail.com>
2012-11-06 18:30:39 -0500

    Created Registration and style pages

---
+++ Registration.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>LOL Registration</title>
+    <link rel="stylsheet" href="" />
+    <script src="" type="text/script"></script>  
+  </head>
+  <body>
+    <header></header>
+    <form></form>
+    <footer></footer>
+  </body>
+</html>
